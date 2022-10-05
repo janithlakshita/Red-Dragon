2c157dc37f20c0a8871ca13276083eb12bfca7c7
janithlakshita <114977187+janithlakshita@users.noreply.github.com>
2022-10-05 09:20:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,7 +29,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+940768632841'] // owner number
+global.vcardowner = ['+94768632841'] // owner number
 global.ownername = "😎ᒎᗩᑎᗩ ᗷᗝ丅❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
@@ -47,8 +47,8 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+940768632841']
-global.ownertag = '+940768632841' //your tag number
+global.owner = ['+94768632841']
+global.ownertag = '+94768632841' //your tag number
 global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
@@ -56,7 +56,7 @@ global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['+940768632841'] //ur premium numbers
+global.premium = ['+94768632841'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
